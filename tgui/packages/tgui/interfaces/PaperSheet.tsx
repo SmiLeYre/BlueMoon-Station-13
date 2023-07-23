7ac1bb748d95fd182fb5e253c2049a0c330be4ec
T6751 <66636084+T6751@users.noreply.github.com>
2023-07-23 11:47:55 +0300

    fix "State initialization should be in a constructor"

--- tgui/packages/tgui/interfaces/PaperSheet.tsx
+++ tgui/packages/tgui/interfaces/PaperSheet.tsx
@@ -112,9 +112,8 @@ const canEdit = (heldItemDetails?: WritingImplement): boolean => {
 const fieldRegex: RegExp = /\[((?:_+))\]/gi;
 
 // Handles the ghost stamp when attempting to stamp paper sheets.
-class PaperSheetStamper extends Component<PaperSheetStamperProps> {
+class PaperSheetStamper extends Component<PaperSheetStamperProps, PaperSheetStamperState> {
   style: null;
-  state: PaperSheetStamperState = { x: 0, y: 0, rotation: 0, yOffset: 0 };
   scrollableRef: RefObject<HTMLDivElement>;
 
   constructor(props, context) {
@@ -122,7 +121,9 @@ class PaperSheetStamper extends Component<PaperSheetStamperProps> {
 
     this.style = null;
     this.scrollableRef = props.scrollableRef;
+    this.state = { x: 0, y: 0, rotation: 0, yOffset: 0 };
   }
+
 
   // Stops propagation of a given event.
   pauseEvent = (e: Event): boolean => {
