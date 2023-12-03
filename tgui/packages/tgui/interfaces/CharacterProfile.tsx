0994cfe45c04b31a3879d8a88d28cc6d0df54ce5
Andrey Fink <lady.ll2011@yandex.ru>
2023-12-03 17:32:20 +0300

    Запрос. Показ хеда в маске.
    Убрана лишняя проверка. Теперь, если видно флавор, то видно и хедшот.

--- tgui/packages/tgui/interfaces/CharacterProfile.tsx
+++ tgui/packages/tgui/interfaces/CharacterProfile.tsx
@@ -61,30 +61,42 @@ export const CharacterProfile = (props, context) => {
         </Tabs>
         <Flex>
           <Flex.Item pl="10px">
-            {!data.is_unknown ? (<CharacterProfileImageElement />)
-              : (<Box />)}
+<<<<<<< Updated upstream
+          {
+            // убрал проверку на ношение маски для отображения хеда
+=======
+            {
+            // убрал проверку
+>>>>>>> Stashed changes
+            }
+         { /*  {!data.is_unknown ? (<CharacterProfileImageElement />)
+              : (<Box />)}*/}
+            <CharacterProfileImageElement />
             <CharacterModelImageElement />
           </Flex.Item>
           <Flex.Item Flex-direction="column" pl="10px" width="100%">
-            <Collapsible title={`Раса - ${data.species_name}`} open>
-              <Section style={{ "white-space": "pre-line" }}>
-                {data.custom_species_lore}
-              </Section>
-            </Collapsible>
-            <Collapsible title="Описание персонажа" open>
+            <Collapsible title="Описание Персонажа" open>
               <Section>
                 <Flex direction="column">
                   {data.flavortext
                     ? (<Flex.Item style={{ "white-space": "pre-line" }}>{data.flavortext}</Flex.Item>)
                     : (<Box />)}
-                  {data.flavortext && data.flavortext_naked
-                    ? (<Divider />)
-                    : (<Box />)}
+                </Flex>
+              </Section>
+            </Collapsible>
+            <Collapsible title="Описание Голого Тела Персонажа" open>
+              <Section>
+                <Flex direction="column">
                   {data.flavortext_naked
                     ? (<Flex.Item style={{ "white-space": "pre-line" }}>{data.flavortext_naked}</Flex.Item>)
                     : (<Box />)}
                   {!data.flavortext && !data.flavortext_naked ? ("Отсутствует") : (<Box />)}
                 </Flex>
+              </Section>
+            </Collapsible>
+            <Collapsible title={`Раса - ${data.species_name}`} open>
+              <Section style={{ "white-space": "pre-line" }}>
+                {data.custom_species_lore}
               </Section>
             </Collapsible>
             <Collapsible title="Внеигровые заметки" open>
